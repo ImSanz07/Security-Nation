1107427b3e268cea444bc2f96ea2d1232ba7765b
Sanat Patil <103813994+ImSanz07@users.noreply.github.com>
2023-04-29 03:02:03 +0530

    Creating New directory

--- security_nation/website/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("home",views.home,name="home"),
-    path("login",views.login,name="login"),
-    path("signup",views.signup,name="signup"),
-    path("org_signup",views.org_signup,name="org_signup")
-]
