97364a01d23c807bbade34eaca4675e110561e4f
JoelNaz <113906521+JoelNaz@users.noreply.github.com>
2023-04-28 16:18:56 +0530

    2commit

--- security_nation/website/models.py
+++ security_nation/website/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class userm(models.Model):
+    id = models.AutoField(primary_key=True)
+    uname = models.CharField(max_length=30 , null=True)
+    email = models.CharField(max_length=30 , null=True)
+    passw = models.CharField(max_length=30 , null=True)
+
+    
